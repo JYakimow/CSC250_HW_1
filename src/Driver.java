ace781cdc107f86c07a38e668be4a42dd5cf29a4
jonnyhappyfeet <55300110+jonnyhappyfeet@users.noreply.github.com>
2020-09-04 22:12:33 -0500

    Update Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -2,6 +2,27 @@ public class Driver
 {
 	public static void main(String[] args)
 	{
-		System.out.print("Hello World");
+		System.out.println("Hello World");
+		
+		String aString = "Galations";
+		System.out.println(aString);
+		String Vowels = "AEIOUaeiou";
+		System.out.println(Vowels);
+		
+		for(int i = aString.length(); i >= 0; i++)
+		{
+			
+		}
+		
 	}
+	
+	/*
+	String aString1 = "";
+	String aString2 = "";
+	
+	static String compareStrings(String aString1, String aString2)
+	{
+		return aString1;
+	}
+	*/
 }
